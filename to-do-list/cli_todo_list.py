06189750e346068c14f78c4d1455e292b5071ffe
sachin hegde <sachinparameshwar@gmail.com>
2025-05-23 20:18:53 +0530

    to-do list initial setup

---
+++ to-do-list/cli_todo_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+A simple command-line to-do list application.
+"""
+import argparse
+import os
+import sys
+import datetime
+import json
+
+def get_user_input():
+
+def add_task():
+    
+def delete_task():
+    
+def update_task():
+    
+def list_task():
+    
+def task_progress():
+
+def main():
+    
+
+if __name__ == "__main__":
+    main()
